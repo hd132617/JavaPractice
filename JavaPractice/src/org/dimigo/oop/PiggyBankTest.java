4353ffbdd306c49fce707c47b6a848cf10c61819
leeseunghang <leeseunghang@172.17.0.221>
2015-06-15 16:22:14 +0900

    rs

---
+++ JavaPractice/src/org/dimigo/oop/PiggyBankTest.java
@@ -0,0 +1,25 @@
+package org.dimigo.oop;
+
+public class PiggyBankTest {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		FamilyMember member1 = new FamilyMember("아빠");
+		FamilyMember member2 = new FamilyMember("엄마");
+		FamilyMember member3 = new FamilyMember("나");
+		FamilyMember member4 = new FamilyMember("남동생");
+		
+		FamilyMember.printMemberCnt();
+		PiggyBank.putMoney(member1, 10000);
+		PiggyBank.putMoney(member2, 5000);
+		PiggyBank.putMoney(member3, 2000);
+		PiggyBank.putMoney(member4, 1000);
+		
+		PiggyBank.printBalance();
+		PiggyBank.putMoney(member3, 1000);
+		PiggyBank.printBalance();
+		
+		
+	}
+
+}
