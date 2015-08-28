4c674192c05d96cb3616381a88e352772b3a4afa
leeseunghang <leeseunghang@172.17.0.71>
2015-08-28 16:41:50 +0900

    s

---
+++ JavaPractice/src/org/dimigo/inheritance/SmartPhoneTest.java
@@ -0,0 +1,23 @@
+package org.dimigo.inheritance;
+
+public class SmartPhoneTest {
+
+	public static void main(String[] args) {
+		SmartPhone[] phones = {
+				new IPhone("iPhone6", "애플", 700000),
+				new Galaxy("갤럭시 S6", "삼성", 650000)
+		};
+		
+		for(SmartPhone phone : phones) {
+			System.out.println(phone);
+			phone.turnOn();
+			phone.pay();
+			
+			phone.useSpecialFunction();
+			phone.turnOff();
+			System.out.println();
+			
+		}
+	}
+
+}
